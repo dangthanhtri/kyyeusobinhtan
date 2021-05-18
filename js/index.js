f81683016de4899a44d6e9e72687f5044514b852
dangthanhtri <dangthanhtrispkt@gmail.com>
2021-05-19 00:12:01 +0700

    update bug reset

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/*
 const menu = document.querySelector(".menu");
 const navOpen = document.querySelector(".hamburger");
 const navClose = document.querySelector(".close");
@@ -30,7 +31,7 @@ window.addEventListener("scroll", () => {
     navBar.classList.remove("fix-nav");
   }
 });
-
+*/
 // Scroll To
 /*
 const links = [...document.querySelectorAll(".scroll-link")];
@@ -56,12 +57,7 @@ links.map(link => {
   });
 });
 */
-//gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
-//gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
-//gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
-//gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
-//gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
-//gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+
 
 function openMainScr() {
   document.getElementById("personScr").style.display = "none";
